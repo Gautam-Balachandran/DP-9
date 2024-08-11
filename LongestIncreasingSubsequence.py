ead26c01d9660cbf4c3fd31b7a6aa16f138fb1c5
Gautam-Balachandran <gautam.balachandran@gmail.com>
2024-08-11 09:27:16 -0400

    DP-9 Completed

---
+++ LongestIncreasingSubsequence.py
@@ -0,0 +1,36 @@
+﻿# Time Complexity : O(n^2), where n is the length of the input array
+# Space Complexity : O(n)
+class Solution:
+    def lengthOfLIS(self, nums):
+        if len(nums) == 0:
+            return 0
+        
+        maxLength = 0
+        dp = [1] * len(nums)  # Min length is 1
+        
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        
+        for length in dp:
+            maxLength = max(maxLength, length)
+        
+        return maxLength
+
+# Examples
+
+# Example 1
+nums = [10, 9, 2, 5, 3, 7, 101, 18]
+sol = Solution()
+print(sol.lengthOfLIS(nums))  # Output: 4
+
+# Example 2
+nums = [0, 1, 0, 3, 2, 3]
+sol = Solution()
+print(sol.lengthOfLIS(nums))  # Output: 4
+
+# Example 3
+nums = [7, 7, 7, 7, 7, 7, 7]
+sol = Solution()
+print(sol.lengthOfLIS(nums))  # Output: 1
---
+++ RussianDollEnvelopes.py
@@ -0,0 +1,45 @@
+﻿# Time Complexity : O(nlogn), where n is the number of envelopes
+# Space Complexity : O(n)
+from bisect import bisect_left
+
+class Solution:
+    def maxEnvelopes(self, envelopes):
+        if not envelopes or len(envelopes[0]) != 2:
+            return 0
+        
+        # Sort the envelopes first by width in ascending order.
+        # If the width is the same, sort by height in descending order.
+        envelopes.sort(key=lambda x: (x[0], -x[1]))
+        
+        # Extract the heights and find the length of the longest increasing
+        # subsequence
+        heights = [envelope[1] for envelope in envelopes]
+        
+        return self.lengthOfLIS(heights)
+    
+    def lengthOfLIS(self, nums):
+        dp = []
+        for num in nums:
+            i = bisect_left(dp, num)
+            if i < len(dp):
+                dp[i] = num
+            else:
+                dp.append(num)
+        return len(dp)
+
+# Examples
+
+# Example 1
+envelopes = [[5, 4], [6, 4], [6, 7], [2, 3]]
+sol = Solution()
+print(sol.maxEnvelopes(envelopes))  # Output: 3
+
+# Example 2
+envelopes = [[1, 1], [1, 1], [1, 1]]
+sol = Solution()
+print(sol.maxEnvelopes(envelopes))  # Output: 1
+
+# Example 3
+envelopes = [[4, 5], [4, 6], [6, 7], [2, 3], [1, 1]]
+sol = Solution()
+print(sol.maxEnvelopes(envelopes))  # Output: 4
